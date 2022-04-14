6460bc30c9e5b6dc62280bffdc23c5ae30b51faf
sohee kim <sohee063@nate.com>
2022-04-14 15:35:05 +0900

    four commit

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let todoList=document.getElementById("todo-list")
 let addButton=document.getElementById("add-button")
 let taskList=[]
+let clickall=document.getElementById("all")
+let clicking=document.getElementById("ing")
+let clickfinished=document.getElementById("finished")
+let taskAll=[]
+let taskIng=[]
+let taskFinished=[]
 
 addButton.addEventListener("click",addTask)
 todoList.addEventListener("focus",blank)
+clickall.addEventListener("click",render)
+clicking.addEventListener("click",alling)
+clickfinished.addEventListener("click",allfinished)
+
 
 function addTask(){
     let task ={
@@ -12,6 +22,7 @@ function addTask(){
         isComplete:false
     };
     taskList.push(task);
+    taskFinished.push(task)
     console.log(taskList);
     render();
 }
@@ -27,6 +38,7 @@ function render(){
                 <i onclick="deleteTask('${taskList[i].id}')" class="fa-solid fa-trash-can fa-2x"></i>
             </div>
         </div>`;
+      
         }else{
             resultHTML +=`<div class="task">
             <div>${taskList[i].taskContent}</div>
@@ -40,16 +52,21 @@ function render(){
     document.getElementById("task-board").innerHTML = resultHTML;
 }
 
+
 function toggleComplete(id){
     console.log("id",id);
     for(let i=0;i<taskList.length;i++){
         if(taskList[i].id == id){
             taskList[i].isComplete = !taskList[i].isComplete;
+            taskIng.push(taskList[i])
+            
             break;
         }
     }
+    
+    
+    console.log(taskIng);
     render();
-    console.log(taskList);
 }
 
 
@@ -68,8 +85,45 @@ function blank(){
 }
 
 
+function alling(){
+    let resultHTML = "";
+    for(let i=0;i<taskIng.length;i++){
+        resultHTML+=`<div class="task">
+        <div>${taskIng[i].taskContent}</div>
+        <div class="icons">
+            <i onclick="toggleComplete('${taskIng[i].id}')"class="fa-solid fa-check fa-2x"></i>
+            <i onclick="deleteTask('${taskIng[i].id}')" class="fa-solid fa-trash-can fa-2x"></i>
+        </div>
+    </div>`;
+    console.log(`id는${taskIng[i].id}`);
+    }
+    
+    document.getElementById("task-board").innerHTML = resultHTML;
+    
+}
+
+function allfinished(){
+    let resultHTML = "";
+    for(let i=0;i<taskList.length;i++){
+        if(!taskList[i].isComplete){
+            taskFinished.push(taskList[i]);
+            resultHTML+=`<div class="task">
+            <div>${taskFinished[i].taskContent}</div>
+            <div class="icons">
+                <i onclick="toggleComplete('${taskFinished[i].id}')"class="fa-solid fa-check fa-2x"></i>
+                <i onclick="deleteTask('${taskFinished[i].id}')" class="fa-solid fa-trash-can fa-2x"></i>
+            </div>
+        </div>`;
+        console.log(`id는${taskFinished[i].id}`);
+        }
+       
+    
+    }
+    
+    document.getElementById("task-board").innerHTML = resultHTML;
+
+}
+
 function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2, 9);
 }
-
-    
